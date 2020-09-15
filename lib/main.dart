54ede23b26b8f665c431c3cbd14024f3477b258c
Junaidhassan99 <61021884+Junaidhassan99@users.noreply.github.com>
2020-09-15 12:41:50 +0500

    Updates.

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -15,9 +17,65 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+enum DiceType {
+  FirstDice,
+  SecondDice,
+}
+
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int _firstDiceNumber = 1;
+  int _secondDiceNumber = 1;
+  Widget _getdiceElement(DiceType diceType) {
+    String assetImagePath;
+    if (diceType == DiceType.FirstDice) {
+      assetImagePath = 'images/dice$_firstDiceNumber.png';
+    } else {
+      assetImagePath = 'images/dice$_secondDiceNumber.png';
+    }
+    return Expanded(
+      child: GestureDetector(
+        child: Image.asset(assetImagePath),
+        onTap: () {
+          if (diceType == DiceType.FirstDice) {
+            setState(() {
+              do {
+                print('1');
+                _firstDiceNumber = Random().nextInt(6);
+              } while (_firstDiceNumber == 0);
+            });
+          } else {
+            setState(() {
+              do {
+                print('2');
+                _secondDiceNumber = Random().nextInt(6);
+              } while (_secondDiceNumber == 0);
+            });
+          }
+        },
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Padding(
+        padding: EdgeInsets.all(8),
+        child: Row(
+          children: [
+            _getdiceElement(DiceType.FirstDice),
+            SizedBox(
+              width: 4,
+            ),
+            _getdiceElement(DiceType.SecondDice),
+          ],
+        ),
+      ),
+    );
   }
 }
